722d4d310abbe34985a01e33fa168f266a3abbe0
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-03-24 20:04:58 +0100

    ire

---
+++ ireRegistration.py
@@ -0,0 +1,40 @@
+users =[]
+
+for user in range(5):   
+    user_name =input("Register - Enter your username\n")
+    for i in users:
+       while i["user_name"] == user_name:
+            print("Username taken")
+            user_name= input("Enter a different username\n")
+    last_name,password = input("Enter your last name\n"),input("Enter your password\n")
+    while len(password) < 8 :
+        print("Password must be greater than 8 characters")
+        password = input("Enter your password\n")
+
+    myUser = dict(user_name=user_name, last_name=last_name, password=password)
+    users.append(myUser)
+
+# login 
+    print("Let's login and do some hehehe")
+    user_name =input("Enter your username\n")
+    for i in users:
+        if i["user_name"] == user_name:
+            print("user found!")
+            password= input("Enter your password\n")
+            if i["password"] == password:
+                print("Login Successful")
+                break
+            else :    
+                change = input("forgot password?? Enter yes to change password ")
+                if change == "yes":
+                    new_password = input("enter new password\n")
+                    i["password"] = new_password
+                    print(f"your new info: {i}")
+                    break
+        else:
+            print("User not found")
+        
+           
+        
+print(users)
+
