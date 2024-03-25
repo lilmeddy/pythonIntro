203109cdb86a70293fe9b11860e7c7ec93703237
lilmeddy <aghedopriscilla@gmail.com>
2024-03-25 23:31:44 +0100

    Rebecca din have sense at all

--- natCbt.py
+++ natCbt.py
@@ -1,4 +1,4 @@
-cbt = [{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide"],"correctAnswer":"a"},{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide"],"correctAnswer":"b"},{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide"],"correctAnswer":"c"}]
+cbt = [{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide","c)Ayomide","d)"],"correctAnswer":"a"},{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide"],"correctAnswer":"b"},{"question":"1) What is my full name ","option":["a)Osamede","b)Osamide"],"correctAnswer":"c"}]
 correct = ["a","b","c"];score = int(0);userAnswer =[];options = ["a","b","c","d"]
 studentName =input("Enter your name\n");regNum =int(input("Enter your regestration number (not less than or greater than 5)\n")); covert = str(regNum)
 while (len(covert)) != 5:
@@ -8,5 +8,19 @@ for questions in cbt:
     print(questions["question"])
     for options in questions["option"]:
         print(options)
-    studentAns = input("Enter your answer (a,b,c,d,e)\n");print("_______________________________");print()
-print()
+    studentAns = input("Enter your answer (a,b,c,d,e)\n"); print("_______________________________");print()
+    print()
+    while studentAns not in correct:
+        studentAns = input("This is why people fail simple instruction you can't follow idiot\nEnter your answer (a,b,c,d,e)\n")
+    userAnswer.append(studentAns)
+    if studentAns == questions["correctAnswer"]:
+       score+=100
+       print(f"Awwn so smart you passed now you have + {score} ") 
+    else:
+        score -=50
+        print(f"Awwwn such an idiot you failed it hehe you can do better lol {score}")
+meanScore = score/5      
+if (meanScore >= 70):
+    print(f"Dear{studentName[:3]}{covert[:3]} your average score is {meanScore}.\nYou Passed cute hehe aburo osamede hehe ")
+else:
+    print(f"Dear{studentName[:3]}{covert[:3]} your average score is {meanScore}.\nIdiot you failed sad hehe olodo try again next year mumu aburo ire hahah")
