704c6ddb1bb7c0ca41428204d4c2dc32e04e716f
lilmeddy <aghedopriscilla@gmail.com>
2024-03-27 09:07:08 +0100

    Rebecca din have sense at all

--- tuple.py
+++ tuple.py
@@ -10,3 +10,24 @@ print(tuple(newTuple))
 (first, *second, third) = fruits #note this will print to the end
 print(second)
 print(third)
+
+# create a program to ask for all names(first, *middle*,last)  of five students group all the first name in a list middle in a list and last name in a list 
+first = []
+middle =[]
+last = []
+for i in range(2):
+    firstName = input("Enter your first name (compulsory)\n")
+    while firstName == "":
+        firstName = input("You don't have sense can't you see compulsory \nEnter your first name mumu\n")
+    first.append(firstName)
+    print(first)
+    middleName = input("Enter your middle name (optional)\n")
+    if middleName == "":
+        middleName = "NIL"
+    middle.append(middleName)
+    print(middle)
+    lastName = input("Enter your last name (compulsory)\n")
+    while lastName == "":
+        lastName = input("You don't have sense can't you see compulsory \nEnter your last name mumu\n")
+    last.append(lastName)
+    print(last)
