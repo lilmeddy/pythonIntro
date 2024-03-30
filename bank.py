41b3c097438d9e2f9f185b8da4a13d228e98ba91
lilmeddy <aghedopriscilla@gmail.com>
2024-03-30 08:25:39 +0100

    Rebecca din have sense at all

--- bank.py
+++ bank.py
@@ -10,6 +10,8 @@ fixedBalance = 500000
 phone = []
 user =[]
 for i in range(2):
+    fixedBalance +=fixedBalance
+    accoutBalance.append(fixedBalance)
     firstName = input("Enter your first name (compulsory)\n").capitalize()
     new = False
     while new == False:
@@ -30,11 +32,12 @@ for i in range(2):
         if stress ==False: 
            lastName = input("You don't have sense can't you see compulsory \nEnter your last name mumu\n").capitalize()
     last.append(stress) 
-    fixedBalance *=2
-    accoutBalance.append(fixedBalance)
     starts = ["9","8","7"]
     sec = ["0","1"]
     phoneNumber = input("Enter your phone number\n+234 ")
+    for i in phone:
+        while i == phoneNumber:
+            phoneNumber = input("Phone number taken\nEnter your phone number\n+234 ")
     num = False 
     while num == False:
         for i in phoneNumber.split(" "):
@@ -43,8 +46,7 @@ for i in range(2):
             if num == False:
                 phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
     phone.append(num)
-            
-    user.append([first,last,accoutBalance,phone])
-    
-
+      
+  
+user.extend([first,last,accoutBalance,phone])
 print(user)
