fdff2eb1de43d42a6ebb872bc674d3a0bd9d162b
lilmeddy <aghedopriscilla@gmail.com>
2024-04-03 11:10:08 +0100

    Bank app assignment

--- function.py
+++ function.py
@@ -1,2 +1,29 @@
+# parameterize and none paramenterize
+
+# non-parameterize 
+
 def name():
-    print("hello")
+    print("hello")    
+name()
+for i in range(5):
+    name()
+#parameterize
+
+def name3(name):
+    print(name)
+    
+name3("shade")
+def name3(num):
+    for i in range(num):
+        print("mede")
+name3(11)
+#SHADE is the argument
+name =[]
+def register():
+    fullname = input("enter your name ")
+    name.append(fullname)
+    
+for in range(5):
+    register()
+    
+def 
