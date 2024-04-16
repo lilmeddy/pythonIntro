4aade40b89d5f27c772badee0ea492490df42e13
Dev-Natha <maranathaayilara@gmail.com>
2024-04-16 11:37:56 +0100

    Classes

--- classes.py
+++ classes.py
@@ -5,6 +5,8 @@ class car:
         self.color = color
         self.col = tyres
         self.speed = 0
+        self.move("20")
+        self.greeting("Osamede")
     def move(self,distance):
         self.speed = distance
         print(f"The {self.name} car is moving hehe at {distance}km/hr. The color is {self.color} with {self.col} numbers of tyres ")
@@ -12,5 +14,17 @@ class car:
         print(f"The {self.name} driver {greet} is speeding at {self.speed}km/hr ")
 var = car("Tesla","Red","4")
 # print(var.name)
-var.move("20")
-var.greeting("Osamede")
+
+
+class dog:
+    def __init__(self,name,color,breed) -> None:
+        self.name = name 
+        self.color = color
+        self.breed = breed
+        self.bark("woahhhhhhh")
+    def bark(self,bark):
+        print(f"Osamede's dog '{self.name} a {self.color} {self.breed} ' is barking {bark}")
+var = dog("Roxy","White","Eskimo")
+
+        
+# assigment a class authentication then next register and login 
