7d96b7d1c1a3832ac8d54e091ac69e22e6e7c7ab
Dev-Natha <maranathaayilara@gmail.com>
2024-04-16 10:45:15 +0100

    Classes

--- classes.py
+++ classes.py
@@ -2,5 +2,8 @@ class car:
     # name = "Tesla"
     def __init__(self) -> None:
         self.name = "Tesla"  
+    def move(self):
+        print(f"The {self.name} is moving hehe")
 var = car()
-print(var.name)
+# print(var.name)
+var.move()
