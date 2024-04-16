544f15691ed7f7102615b05671a3912d4316ec69
Dev-Natha <maranathaayilara@gmail.com>
2024-04-16 14:40:31 +0100

    Classes

--- classauth.py
+++ classauth.py
@@ -1,11 +1,14 @@
 class auth:
     def __init__(self) -> None:
+        self.list = []
         self.reg()
         self.log()
     def reg(self):
         name = input("Enter your full name: ")
         phoneNumber = input("Enter your phone number: ")
         password = input("Enter your password: ")
+        self.list.append([name, phoneNumber, password])
+        print(self.list)
     def log(self):
         pass 
 auth()    
