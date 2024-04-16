8e50782f810caf5f5bf457fc58225787feaf1c64
Dev-Natha <maranathaayilara@gmail.com>
2024-04-16 20:40:00 +0100

    Classes

--- classauth.py
+++ classauth.py
@@ -1,6 +1,7 @@
 class auth:
     def __init__(self) -> None:
         self.list = []
+        self.logged =[]
         self.reg()
         self.log()
     def reg(self):
@@ -8,7 +9,27 @@ class auth:
         phoneNumber = input("Enter your phone number: ")
         password = input("Enter your password: ")
         self.list.append([name, phoneNumber, password])
-        print(self.list)
+        print("Enter  ")
     def log(self):
-        pass 
+        username = input("Enter your username")
+        for i in self.list :
+             print(self.list)
+        isNotLoggedIn = True
+        while i[0] != username:
+            print("Invalid username");username = input("Enter your user name\n")
+        print(self.list)
+        passWord= input("Enter your password\n")
+        if passWord == i[2]:
+                self.logged.append([username,passWord]) 
+                print(f"The currently logged in user is {self.logged}")
+                isNotLoggedIn = False
+        else:
+            print("invalid password");change = input("forgot password?? Enter yes to change password ")
+            if change == "yes":
+                i.pop(2)
+                newPassword = input("enter new password\n");i.append(newPassword)
+                print(f"The currently logged in user is {self.logged} with the password changed to")
+
+        
+            
 auth()    
