acc31b086029632b520fb9372c255806ef1a44a0
Dev-Natha <maranathaayilara@gmail.com>
2024-04-16 21:16:42 +0100

    Classes

--- classauth.py
+++ classauth.py
@@ -2,20 +2,26 @@ class auth:
     def __init__(self) -> None:
         self.list = []
         self.logged =[]
+        self.starts =["9","8","7"]
+        self.sec =["0","1"]
         self.reg()
         self.log()
     def reg(self):
         name = input("Enter your full name: ")
         phoneNumber = input("Enter your phone number: ")
+        for i in phoneNumber.split(" "):
+            if i != "" and i[0] in self.starts and i[1] in self.sec and len(i)==10  :
+                num = i
+            if num == False:
+                phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
         password = input("Enter your password: ")
-        self.list.append([name, phoneNumber, password])
-        print("Enter  ")
+        self.list.append([name, num, password])
     def log(self):
         username = input("Enter your username: ")
         for i in self.list :
              print(self.list)
         while i[0] != username:
-            print("Invalid username");username = input("Enter your user name: ")
+            print("Invalid username");username = input("Enter your full name: ")
         print(self.list)
         passWord= input("Enter your password\n")
         while passWord != i[2]:
