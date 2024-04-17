b4df08bc40ecc5708f2027eb586d8845f5ea283d
Dev-Natha <maranathaayilara@gmail.com>
2024-04-17 10:56:52 +0100

    Classes

--- inheritance.py
+++ inheritance.py
@@ -1,2 +1,9 @@
 class person:
+    def __init__(self,first,last) -> None:
+        self.first = first
+        self.last = last
     
+    def greeting(self):
+        print(f"My name is {self.first} {self.last}")
+
+class student(human)
