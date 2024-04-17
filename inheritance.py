df26e28f4e9ee90d64c4baaec4712cbbc0b9cfee
Dev-Natha <maranathaayilara@gmail.com>
2024-04-17 11:02:02 +0100

    Classes

--- inheritance.py
+++ inheritance.py
@@ -5,5 +5,10 @@ class person:
     
     def greeting(self):
         print(f"My name is {self.first} {self.last}")
-
-class student(human)
+class student(person):
+    pass 
+class studentchild(student):
+    pass
+# var = student("mede","ede")
+var = studentchild("mede","ede")
+var.greeting()
