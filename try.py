79a413cef5cd6e075fd5c444f31e7a5f22a4f54e
Dev-Natha <maranathaayilara@gmail.com>
2024-04-18 11:00:11 +0100

    Classes

--- try.py
+++ try.py
@@ -1,5 +1,14 @@
+# try:
+#     int(input("Enter a number "))
+# except:
+#     print("There's an error here")
+# else:
+#     print("There is no error here")
+# finally:
+#     print("Hehehe")
+    
 try:
-    int("mede")
+    int(input("Enter a number "))
 except:
     print("There's an error here")
 else:
