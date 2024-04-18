99afcbee8d2b2608f9ee6743ca05cbcfbfcd01ad
Dev-Natha <maranathaayilara@gmail.com>
2024-04-18 14:42:02 +0100

    Classes

--- try.py
+++ try.py
@@ -6,13 +6,25 @@
 #     print("There is no error here")
 # finally:
 #     print("Hehehe")
-    
-try:
-    int(input("Enter a number "))
-except:
-    print("There's an error here")
-else:
-    print("There is no error here")
-finally:
-    print("Hehehe")
+# while True:   
+#     try:
+#         int(input("Enter a number "))
+#     except:
+#         print("There's an error here")
+#     else:
+#         print("There is no error here")
+#         break
+
+while True:   
+    try:
+        int(input("Enter a number "))
+        try:
+            var1 = 2 + "2"
+        except:
+            print("You can't add both together ")
+    except:
+        print("There's an error here")
+    else:
+        print("There is no error here")
+        break
     
