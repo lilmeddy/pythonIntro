73a7522ac0099cc69d650a54571ebaa97d036316
Dev-Natha <maranathaayilara@gmail.com>
2024-04-21 16:26:58 +0100

    Classes

--- classBank.py
+++ classBank.py
@@ -4,19 +4,21 @@ class bank:
         self.start()
         self.balance = 5000
         self.phone = []
+        # self.register()
     def start(self):
         print("Welcome to our bank\nEnter 1 to register\nEnter 2 to login\nEnter 3 to quit\n")
-        try:
-            ans = int(input(""))
-            if ans in ["1", "2", "3"]:
-                if ans == "1":
-                    self.register()
-                if ans == "2":
-                    self.login()
-        except:
-            ans = int(input(""))
-        else:
-            break
+        while True:
+            try:
+                ans = int(input(""))
+                if ans in ["1", "2", "3"]:
+                    if ans == "1":
+                        self.register()
+                    if ans == "2":
+                        self.login()
+            except:
+                print("Must contain only numbers from 1-3")
+            else:
+                break
     def register(self):
         self.balance +=50000;firstName = input("Enter your first name (compulsory)\n").capitalize();first = False
         while first == False:
@@ -33,24 +35,26 @@ class bank:
                     last = i
             if last ==False: 
                lastName = input("Enter your last name (compulsory)\n").capitalize()
-        try:
-            phoneNumber = int(input("Enter your phone number\n+234 "))
-            starts = ["9","8","7"];sec = ["0","1"]
-            num = False 
-            while num == False:
-                for i in self.phone:
-                    while i == phoneNumber:
-                        phoneNumber = input("Phone number taken\nEnter your phone number\n+234 ")
-                for i in phoneNumber.split(" "):
-                    if i != "" and i[0] in starts and i[1] in sec and len(i)==10  :
-                        num = i
-                    if num == False:
-                        phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
-        except:
-            phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
-        else:
-            break
-            
+        while True:
+            try:
+                phoneNumber = int(input("Enter your phone number\n+234 "))
+                starts = ["9","8","7"];sec = ["0","1"]
+                num = False 
+                while num == False:
+                    for i in self.phone:
+                        while i == phoneNumber:
+                            phoneNumber = input("Phone number taken\nEnter your phone number\n+234 ")
+                    for i in phoneNumber.split(" "):
+                        if i != "" and i[0] in starts and i[1] in sec and len(i)==10  :
+                            num = i
+                        if num == False:
+                            phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
+            except:
+                phoneNumber = input("All nigerians phone number +234 (and should start with 9,8 or 7 and the second value should be 0 or 1) and 10 digits\nEnter your phone number \n+234 ")
+            else:
+                break
+        
+bank()          
          
         
         
